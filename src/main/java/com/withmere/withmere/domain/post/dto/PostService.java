925250812dbdda8017b3d0a416f0cd7aeb1d0828
MsEmily1020 <peace10200khs@gmail.com>
2024-06-09 00:50:57 +0900

    feat: save post service 추가

---
+++ src/main/java/com/withmere/withmere/domain/post/dto/PostService.java
@@ -0,0 +1,21 @@
+package com.withmere.withmere.domain.post.dto;
+
+import com.withmere.withmere.domain.post.PostRepository;
+import com.withmere.withmere.domain.user.UserRepository;
+import com.withmere.withmere.global.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public PostResponse save(AddPostRequest request) {
+        if(!userRepository.existsById(request.getUser().getId())) throw UserNotFoundException.EXCEPTION;
+        return new PostResponse(postRepository.save(request.toEntity()));
+    }
+}
