7056885b589cde42d3275a1f2e047dbd02a080c6
MsEmily1020 <peace10200khs@gmail.com>
2024-06-08 23:42:10 +0900

    add: security config 추가

---
+++ src/main/java/com/withmere/withmere/global/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.withmere.withmere.global.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/static/**");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrf) -> csrf.disable())
+                .cors((cors) -> cors.disable())
+                .formLogin((formLogin) -> formLogin.disable())
+                .headers((headers) -> headers.disable());
+
+        return http.build();
+    }
+}
