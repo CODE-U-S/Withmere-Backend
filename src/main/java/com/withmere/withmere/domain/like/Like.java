5d7aaf0088f5074d9352d936cbdd8e61d46e1510
MsEmily1020 <peace10200khs@gmail.com>
2024-06-09 02:35:19 +0900

    add: like entity 추가

---
+++ src/main/java/com/withmere/withmere/domain/like/Like.java
@@ -0,0 +1,31 @@
+package com.withmere.withmere.domain.like;
+
+import com.withmere.withmere.domain.post.Post;
+import com.withmere.withmere.domain.user.User;
+import com.withmere.withmere.global.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Like extends BaseEntity {
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Builder
+    public Like(User user, Post post) {
+        this.user = user;
+        this.post = post;
+    }
+}
