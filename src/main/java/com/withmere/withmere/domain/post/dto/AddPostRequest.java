b0457ba54f6aba8b46f491776846478d86a5c34b
MsEmily1020 <peace10200khs@gmail.com>
2024-06-09 00:44:45 +0900

    add: post request 추가

--- src/main/java/com/withmere/withmere/domain/post/dto/AddPostRequest.java
+++ src/main/java/com/withmere/withmere/domain/post/dto/AddPostRequest.java
@@ -1,6 +1,11 @@
 package com.withmere.withmere.domain.post.dto;
 
+import com.withmere.withmere.domain.post.Category;
+import com.withmere.withmere.domain.post.Field;
+import com.withmere.withmere.domain.post.Post;
+import com.withmere.withmere.domain.user.User;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,4 +14,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddPostRequest {
+    @NotEmpty(message = "제목 칸이 비어있습니다.")
+    private String title;
+
+    private String content;
+
+    @NotNull(message = "user의 id를 넣어주세요.")
+    private User user;
+
+    private Category category;
+
+    private Field field;
+
+    public Post toEntity() {
+        return Post.builder()
+                .title(title)
+                .content(content)
+                .user(user)
+                .category(category)
+                .field(field)
+                .build();
+    }
 }
