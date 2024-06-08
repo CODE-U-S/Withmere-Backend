7e66872a2e9c413c988c1a27d8636642f4ce371d
MsEmily1020 <peace10200khs@gmail.com>
2024-06-08 23:43:29 +0900

    WebMvcConfig 추가

---
+++ src/main/java/com/withmere/withmere/global/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.withmere.withmere.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:src/main/resources/static/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://127.0.0.1:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
