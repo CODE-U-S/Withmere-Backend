a199f84839c3278d187049a954095f17fcdb88f1
MsEmily1020 <peace10200khs@gmail.com>
2024-06-09 00:41:49 +0900

    add: global exception handler 추가

--- src/main/java/com/withmere/withmere/domain/post/dto/AddPostRequest.java
+++ src/main/java/com/withmere/withmere/domain/post/dto/AddPostRequest.java
@@ -1,5 +1,6 @@
 package com.withmere.withmere.domain.post.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
