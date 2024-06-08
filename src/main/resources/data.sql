4e24dda6ddfecbc0e7e6a76b3675a66e0c88bf8b
MsEmily1020 <peace10200khs@gmail.com>
2024-06-08 23:50:55 +0900

    fix: user base entity id import 변경 및 data.sql 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO user VALUES('rlagPtmd', 'rlagPtmd@e-mirim.hs.kr', '92857af8846936f0352f6ae14fb1e8703466e19d', 0);
+INSERT INTO user(name, email, pw, category, created_date) VALUES('rlagPtmd', 'rlagPtmd@e-mirim.hs.kr', '92857af8846936f0352f6ae14fb1e8703466e19d', 'DEVELOPER', '2023-10-20 10:11:20');
