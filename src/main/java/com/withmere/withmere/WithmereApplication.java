50a075332ebc14bc7cd063e404a01aff5698a84a
MsEmily1020 <peace10200khs@gmail.com>
2024-06-08 23:02:56 +0900

    init: 프로젝트 초기 셋팅

---
+++ src/main/java/com/withmere/withmere/WithmereApplication.java
@@ -0,0 +1,13 @@
+package com.withmere.withmere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithmereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithmereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/withmere/withmere/WithmereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withmere.withmere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithmereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
