291d8d0819f025cd527ab9de72b29cb9af3eef0f
MsEmily1020 <peace10200khs@gmail.com>
2024-06-12 08:28:58 +0900

    feat: @EnableJpaAuditing 추가

--- src/main/java/com/withmere/withmere/WithmereApplication.java
+++ src/main/java/com/withmere/withmere/WithmereApplication.java
@@ -2,7 +2,10 @@ package com.withmere.withmere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class WithmereApplication {
 
