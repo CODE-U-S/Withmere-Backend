e8059aa281bc3af3347ea5c36eba5ab383aeaa28
MsEmily1020 <peace10200khs@gmail.com>
2024-06-13 10:18:34 +0900

    feat: data.sql user 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO user(name, email, pw, category, created_date) VALUES('rlagPtmd', 'rlagPtmd@e-mirim.hs.kr', '92857af8846936f0352f6ae14fb1e8703466e19d', 'DEVELOPER', '2023-10-20 10:11:20');
+INSERT INTO user(name, email, pw, category, created_date) VALUES('dlwldls', 'dlwldls@e-mirim.hs.kr', '92857af8846936f0352f6ae14fb1e8703466e19d', 'DEVELOPER', '2023-10-20 10:11:20');
+INSERT INTO user(name, email, pw, category, created_date) VALUES('dlwlstjs', 'dlwlstjs@e-mirim.hs.kr', '92857af8846936f0352f6ae14fb1e8703466e19d', 'DEVELOPER', '2023-10-20 10:11:20');
