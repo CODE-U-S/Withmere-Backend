0303f51ad675970fa6b6b98689e07a39cedf8e68
MsEmily1020 <peace10200khs@gmail.com>
2024-06-13 10:23:43 +0900

    feat: post repository status 조회 메소드 추가

--- src/main/java/com/withmere/withmere/domain/post/PostRepository.java
+++ src/main/java/com/withmere/withmere/domain/post/PostRepository.java
@@ -6,5 +6,7 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByCategory(Category category);
+
+    List<Post> findAllByStatus(Status status);
     List<Post> findAllByTitleContainingOrContentContaining(String title, String content);
 }
