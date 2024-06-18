af1befc27cc2850912b690d2972b60f0fc89006c
MsEmily1020 <peace10200khs@gmail.com>
2024-06-18 17:47:08 +0900

    add: comment response createdDate 추가

--- src/main/java/com/withmere/withmere/domain/comment/dto/CommentResponse.java
+++ src/main/java/com/withmere/withmere/domain/comment/dto/CommentResponse.java
@@ -5,17 +5,21 @@ import com.withmere.withmere.domain.post.Post;
 import com.withmere.withmere.domain.user.User;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class CommentResponse {
     private final Long id;
     private final User user;
     private final Post post;
     private final String comment;
+    private final LocalDateTime createdDate;
 
     public CommentResponse(Comment comment) {
         this.id = comment.getId();
         this.user = comment.getUser();
         this.post = comment.getPost();
         this.comment = comment.getComment();
+        this.createdDate = comment.getCreatedDate();
     }
 }
